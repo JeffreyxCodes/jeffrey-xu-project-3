b48ebf70d458e7ec22fe76470e9926a990673741
jeffreyxdc <jeffreyxdc@gmail.com>
2019-05-17 04:40:02 -0400

    got player moving all the way to the end tile with click

--- scripts/script.js
+++ scripts/script.js
@@ -1,116 +1,170 @@
-$(function() {
-    const game = {
-        grid: [],
-        // 0: field tile - black
-        // 1: untrigger tile - orange
-        // 2: triggered tile - firebrick
-        // 3: start tile - gray
-        // 4: end tile - green
-        path: [],
-        pathTiles: 0,
-        player: [],
-        $container: $('.game-container'),
+const game = {
+    grid: [],
+    // 0: field tile - black
+    // 1: untrigger tile - orange
+    // 2: triggered tile - firebrick
+    // 3: start tile - gray
+    // 4: end tile - green
+    path: [],
+    pathTiles: 0,
+    player: [],
+    score: 0,
+    $container: $(`.game-container`),
 
-        resetGrid: function() {
-            for (let i = 0; i < 10; i++) {
-                this.grid.push((new Array(10)).fill(0));
-            };
-        },
+    initGrid: function () {
+        for (let i = 0; i < 10; i++) {
+            this.grid.push((new Array(10)).fill(0));
+        };
+    },
 
-        setStartPosition: function(i, j) {
-            this.path.push([i, j]);
-            this.grid[i][j] = 3;
-            this.player.push(i, j);
-        },
+    setStartPosition: function (x, y) {
+        this.path = []; // for when the game resets
+        this.path.push([x, y]);
+        this.grid[x][y] = 3;
+        this.player = [] // for when the game resets
+        this.player.push(x, y);
+    },
 
-        findFreeTiles: function(x, y) {
-            const freeTiles = [];
-            if (x - 1 >= 0 && this.grid[x - 1][y] === 0) {
-                freeTiles.push([x - 1, y]);
-            }
-            if (x + 1 < this.grid[0].length && this.grid[x + 1][y] === 0) {
-                freeTiles.push([x + 1, y]);
-            }
-            if (y - 1 >= 0 && this.grid[x][y - 1] === 0) {
-                freeTiles.push([x, y - 1]);
-            }
-            if (y + 1 < this.grid[0].length && this.grid[x][y + 1] === 0) {
-                freeTiles.push([x, y + 1]);
-            }
-            return freeTiles;
-        },
+    findFreeTiles: function (x, y) {
+        const freeTiles = [];
+        if (x - 1 >= 0 && this.grid[x - 1][y] === 0) {
+            freeTiles.push([x - 1, y]);
+        }
+        if (x + 1 < this.grid[0].length && this.grid[x + 1][y] === 0) {
+            freeTiles.push([x + 1, y]);
+        }
+        if (y - 1 >= 0 && this.grid[x][y - 1] === 0) {
+            freeTiles.push([x, y - 1]);
+        }
+        if (y + 1 < this.grid[0].length && this.grid[x][y + 1] === 0) {
+            freeTiles.push([x, y + 1]);
+        }
+        return freeTiles;
+    },
 
-        setPath: function() {
-            let [x, y] = this.path[0]; // start point
-            let freeTiles;
+    setPath: function () {
+        let [x, y] = this.path[0]; // start point
+        let freeTiles = this.findFreeTiles(x, y);
 
+        do {
+            [x, y] = freeTiles[Math.floor(Math.random() * freeTiles.length)];
+            this.path.push([x, y]);
+            this.grid[x][y] = 1;
             freeTiles = this.findFreeTiles(x, y);
-            do {
-                [x, y] = freeTiles[Math.floor(Math.random() * freeTiles.length)];
-                this.path.push([x, y]);
-                this.grid[x][y] = 1;
-                freeTiles = this.findFreeTiles(x, y);
-            } while(freeTiles.length !== 0);
-            this.grid[x][y] = 4; // end point
-            this.pathTiles = this.path.length - 1; // number of steps to complete level
-        },
+        } while (freeTiles.length !== 0);
+        this.grid[x][y] = 4; // end point
+        this.pathTiles = this.path.length;
+    },
 
-        drawGrid: function() {
-            let value;
-            for (let i = 0; i < 10; i++) {
-                for (let j = 0; j < 10; j++) {
-                    value = this.grid[i][j];
-                    // storing i,j in id and value respectively instead of together
-                    // because I think accessing two different attributes is more
-                    // efficient than converting a string to an array and vice versa
-                    if (value === 0) {
-                        this.$container.append(
-                            `<div id="${i}" value=${j}></div>`
-                        );
-                    } else if ( value === 1) {
-                        this.$container.append(
-                            `<div class="untrigger" id="${i}" value=${j}></div>`
-                        );
-                    } else if (value === 3) {
-                        this.$container.append(
-                            `<div class="start-tile" id="${i}" value=${j}></div>`
-                        ); 
-                    } else {
-                        this.$container.append(
-                            `<div class="end-tile" id="${i}" value=${j}></div>`
-                        );  
-                    }
+    drawGrid: function () {
+        let value;
+        for (let i = 0; i < 10; i++) { // y axis
+            for (let j = 0; j < 10; j++) { // x axis
+                value = this.grid[j][i];
+                this.drawTile(value, j, i);
+            }
+        }
+    },
+
+    drawTile: function (value, x, y) {
+        if (value === 0) {
+            this.$container.append(
+                `<div data-position="${x},${y}"></div>`
+            );
+        } else if (value === 1) {
+            this.$container.append(
+                `<div class="untrigger" data-position="${x},${y}"></div>`
+            );
+        } else if (value === 3) {
+            this.$container.append(
+                `<div class="start-tile" data-position="${x},${y}"></div>`
+            );
+        } else {
+            this.$container.append(
+                `<div class="end-tile" data-position="${x},${y}"></div>`
+            );
+        }
+    },
+
+    resetGrid: function () {
+        for (let i = 0; i <= this.pathTiles; i++) {
+            this.grid[this.path[i][0]][this.path[i][1]] = 0;
+            this.updateTile("", this.path[i][0], this.path[i][1]);
+        }
+    },
+
+    updateTile: function (type, x, y) {
+        $(`[data-position="${x},${y}"]`).attr("class", type);
+    },
+
+    updateGrid: function() {
+        // update grid for the next round with new values from path
+    },
+
+    drawPlayer: function () {
+        $(`[data-position="${this.player.toString()}"]`).append(
+            `<div class="player"></div>`
+        );
+    },
+
+    removePlayer: function () {
+        $(`.player`).remove();
+    },
+
+    movePlayer: function (type, x, y) {
+        this.pathTiles--;
+        this.grid[x][y] = 2;
+        this.player = [x, y];
+        this.removePlayer();
+        this.updateTile(type, x, y);
+        this.drawPlayer();
+    },
+
+    checkAdjacent: function(direction, eX, eY) {
+        if (direction === 1) { // left or right of the player
+            if (this.grid[eX][eY] === 1) {
+                this.movePlayer("triggered", eX, eY); // step on an untrigger tile
+            } else if (this.pathTiles === 2 && this.grid[eX][eY] === 4) { // able to take last step
+                this.movePlayer("end-tile", eX, eY);
+                console.log("win");
+            }
+        }
+    },
+
+    initListener: function () {
+        this.$container.on("click", (e) => {
+            if (e.target.className !== "player") {
+                let [eX, eY] = e.target.dataset.position.split(",");
+                [eX, eY] = [Number(eX), Number(eY)];
+                const [pX, pY] = this.player;
+                let direction = 0;
+
+                // check if the clicked tile is adjacent to the player
+                if (eX === pX) { // same row as player
+                    direction = Math.abs(eY - pY);
+                    this.checkAdjacent(direction, eX, eY);
+                } else if (eY === pY) { // same column as player
+                    direction = Math.abs(eX - pX);
+                    this.checkAdjacent(direction, eX, eY);
                 }
             }
-        },
+        });
+    },
 
-        drawPlayer: function() {
-            // console.log($(`.game-container div:nth-child(${this.player[0] * this.grid.length + this.player[1] + 1})`));
-            $(`.game-container div:nth-child(${this.player[0] * this.grid.length + this.player[1] + 1})`).append(
-                `<div class="player"></div>`
-            );
-        },
+    init: function () {
+        this.initGrid();
 
-        initListener: function() {
-            this.$container.on("click", function(e) {
-                // check if the clicked tile is adjacent to the player
-                // if () {
+        this.setStartPosition(4, 6);
+        this.setPath();
+        this.drawGrid();
 
-                // }
-            });
-        },
+        this.initListener();
+        this.drawPlayer();
 
-        init: function() {
-            this.resetGrid();
-            this.setStartPosition(4, 4);
-            this.setPath();
-            this.drawGrid();
-            this.initListener();
-            this.drawPlayer();
+        // console.log(this.grid);
+    }
+};
 
-            console.log(this.grid);
-        }
-    };
-
+$(function() {
     game.init();
 });
